efa246ae40ac16de4818c0fd8a22f54b53c25682
Declan Kelly <dkelly.home@gmail.com>
2018-04-27 12:13:15 -0700

    Added mutable pair indexing from slice utility

--- src/utils.rs
+++ src/utils.rs
@@ -29,6 +29,22 @@ impl<'a, T: 'a> WindowsMut<'a, T> {
     }
 }
 
+pub fn get_pair_mut<'a, T: 'a>(data: &'a mut [T], idx_a: usize, idx_b: usize) -> (&'a mut T, &'a mut T) {
+    if idx_a >= data.len() || idx_b >= data.len() {
+        panic!("Attempted to index beyond bounds of array");
+    } else if idx_a == idx_b {
+        panic!("Attempted to mutably alias same element twice");
+    } else {
+        unsafe {
+            let data_ptr = data.as_mut_ptr();
+
+            let item_a = data_ptr.offset(idx_a as isize).as_mut().unwrap();
+            let item_b = data_ptr.offset(idx_b as isize).as_mut().unwrap();
+            (item_a, item_b)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
