36abbbeb8193882a5235d98e0f56aebe04c1832a
Declan Kelly <dkelly.home@gmail.com>
2018-05-06 20:17:58 -0700

    Add more unstable features and remove unnecessary utility functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-#![feature(exact_chunks, nll, pointer_methods, nonnull_cast)]
+#![feature(exact_chunks, nll, pointer_methods, nonnull_cast, crate_in_paths, box_into_raw_non_null)]
 
 pub mod exact_cover;
-
 pub mod sudoku;
-
 pub mod utils;
--- src/utils.rs
+++ src/utils.rs
@@ -1,35 +1,8 @@
-use std::slice;
-
-pub struct WindowsMut<'a, T: 'a> {
+pub fn get_pair_mut<'a, T: 'a>(
     data: &'a mut [T],
-    current_idx: usize,
-    size: usize,
-}
-
-impl<'a, T: 'a> WindowsMut<'a, T> {
-    pub fn new(data: &'a mut [T], size: usize) -> Self {
-        WindowsMut {
-            data,
-            size,
-            current_idx: 0,
-        }
-    }
-
-    pub fn next(&mut self) -> Option<&mut [T]> {
-        if self.current_idx + self.size > self.data.len() {
-            None
-        } else {
-            unsafe {
-                let data_ptr = self.data.as_mut_ptr().offset(self.current_idx as isize);
-
-                self.current_idx += 1;
-                Some(slice::from_raw_parts_mut(data_ptr, self.size))
-            }
-        }
-    }
-}
-
-pub fn get_pair_mut<'a, T: 'a>(data: &'a mut [T], idx_a: usize, idx_b: usize) -> (&'a mut T, &'a mut T) {
+    idx_a: usize,
+    idx_b: usize,
+) -> (&'a mut T, &'a mut T) {
     if idx_a >= data.len() || idx_b >= data.len() {
         panic!("Attempted to index beyond bounds of array");
     } else if idx_a == idx_b {
@@ -42,23 +15,5 @@ pub fn get_pair_mut<'a, T: 'a>(data: &'a mut [T], idx_a: usize, idx_b: usize) ->
             let item_b = data_ptr.offset(idx_b as isize).as_mut().unwrap();
             (item_a, item_b)
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn windows_over_vec() {
-        let mut data = vec![1, 2, 3, 4, 5];
-
-        let mut windows = WindowsMut::new(&mut data, 2);
-
-        assert_eq!(windows.next().unwrap(), &mut [1, 2]);
-        assert_eq!(windows.next().unwrap(), &mut [2, 3]);
-        assert_eq!(windows.next().unwrap(), &mut [3, 4]);
-        assert_eq!(windows.next().unwrap(), &mut [4, 5]);
     }
 }
