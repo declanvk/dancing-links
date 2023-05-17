95800b884e4a34b6412de0c9076f4c9db1ffd729
Declan <dkelly.home@gmail.com>
2023-05-16 18:04:13 -0700

    Add many regression tests and sudoku examples
    **Description**
     - Add regression tests for issue #4, currently runs very slowly
     - Add data files containing many sudoku games with expected
       solutions
    
    **Motivation**
    This tests should guard against future regressions

---
+++ tests/regression_tests.rs
@@ -0,0 +1,22 @@
+mod common;
+
+use common::parse_sudoku_possibilities;
+use dancing_links::{ExactCover, Solver};
+
+#[test]
+fn issue_4() {
+    env_logger::init();
+
+    // Derived from https://github.com/declanvk/dancing-links/issues/4#issue-1006609105
+    let sudoku_input =
+        "300080900000340000008005600500104070002009010003000040005001200000000000070008090";
+
+    let (puzzle, _) = parse_sudoku_possibilities(sudoku_input, 3);
+    log::debug!("Possibilities:\n{:?}", puzzle.possibilities());
+    log::debug!("Constraints:\n{:?}", puzzle.constraints());
+    let mut solver = Solver::new(&puzzle);
+
+    let solutions = solver.next_solution();
+
+    assert!(solutions.is_some());
+}
